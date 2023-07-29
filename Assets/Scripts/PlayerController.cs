c9508c55274087db068f9f4a4436bbcf87715e63
Baeg'Ehl <bagelyeeter69420@gmail.com>
2023-07-29 14:32:59 -0500

    Player Arms

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject[] limbs;
     [SerializeField] private Rigidbody2D[] limbRBs;
     [SerializeField] private float legSpeed;
+    [SerializeField] private float armSpeed;
+    [SerializeField] private int playerAttackState;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +22,24 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate(){
         MovePlayer();
+        PlayerArms();
     }
     void MovePlayer(){
         if(Input.GetKey(KeyCode.D)){limbRBs[0].AddTorque(Time.fixedDeltaTime * legSpeed);}
         if(Input.GetKey(KeyCode.E)){limbRBs[1].AddTorque(Time.fixedDeltaTime * legSpeed);}
         if(Input.GetKey(KeyCode.A)){limbRBs[0].AddTorque(-Time.fixedDeltaTime * legSpeed);}
         if(Input.GetKey(KeyCode.Q)){limbRBs[1].AddTorque(-Time.fixedDeltaTime * legSpeed);}
+    }
+    void PlayerArms(){
+        switch(playerAttackState){
+            case 1:
+            if(Input.GetMouseButton(0)){limbRBs[2].AddTorque(Time.fixedDeltaTime * armSpeed);}
+            if(Input.GetMouseButton(1)){limbRBs[2].AddTorque(-Time.fixedDeltaTime * armSpeed);}
+            break;
+            case 2:
+            if(Input.GetMouseButton(0)){limbRBs[3].AddTorque(Time.fixedDeltaTime * armSpeed);}
+            if(Input.GetMouseButton(1)){limbRBs[3].AddTorque(-Time.fixedDeltaTime * armSpeed);}
+            break;
+        }
     }
 }
